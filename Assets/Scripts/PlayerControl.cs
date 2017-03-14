e674e866c50033c9ccf389665ae1c3539ec24abe
Ryan Kramer <kramer545@gmail.com>
2017-03-13 17:03:44 -0700

    moved camera angle, added weapon and aligned it, added wasd controls to move and set up blend tree for movement, though clunky right now

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		anim.SetFloat ("x", 0);
+		anim.SetFloat ("z", 0);
+
+		if(Input.GetKey("w"))
+		{
+			this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x,this.gameObject.transform.position.y,this.gameObject.transform.position.z+0.1F);
+			anim.SetFloat ("z", 1);
+		}
+		if(Input.GetKey("a"))
+		{
+			this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x-0.1F,this.gameObject.transform.position.y,this.gameObject.transform.position.z);
+			anim.SetFloat ("x", -1);
+		}
+		if(Input.GetKey("s"))
+		{
+			this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x,this.gameObject.transform.position.y,this.gameObject.transform.position.z-0.1F);
+			anim.SetFloat ("z", -1);
+		}
+		if(Input.GetKey("d"))
+		{
+			this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x+0.1F,this.gameObject.transform.position.y,this.gameObject.transform.position.z);
+			anim.SetFloat ("x", 1);
+		}
+		if(Input.GetMouseButtonDown(0))//left click
+		{
+			this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x+0.1F,this.gameObject.transform.position.y,this.gameObject.transform.position.z);
+		}
+	}
+}
